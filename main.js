2fd0548b302ba8b21d8a2954d086fcb1d9c073cf
unknown <pqcamh228@gmail.com>
2022-02-27 16:41:33 +0800

    20220227

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ const client = new Discord.Client({intents:["GUILDS","GUILD_MESSAGES","GUILD_PRE
 
 // connect to mysql
 var dbContext = mysql.createConnection(appSettings['mysqlConnection']);
-
-
 dbContext.connect(function(err) {
     if (err) throw err;
 
@@ -15,8 +13,8 @@ dbContext.connect(function(err) {
 
     // create a query to insert a record to audit table and set time = now, type = 1(server up)
     let timenow = new Date().toISOString().slice(0, 19).replace("T", " ");
-    let sql = "INSERT INTO actionaudit(audit_id,actiontime,actiontype_id) VALUES(default,'" 
-                + timenow + "',1);";
+    let sql = "INSERT INTO actionaudit VALUES(default,'" 
+                + timenow + "',1, NULL, 'client')";
 
     // execute the query
     dbContext.query(sql, function(err, result){
@@ -58,8 +56,62 @@ client.on('messageCreate', (message) =>
 // needs GUILD_VOICE_STATES option to work when initializing Discord.Client
 client.on('voiceStateUpdate', (oldState, newState) =>
 {
-    console.log("voicechannel");
+    // 退出語音時，newstate中的channel id會為空
+    if(newState.channelId == undefined)
+        return;
+
+    //若非退出語音(也就是加入語音), 做下面的事情
+    dbContext.connect(function(err){
+
+        const sql = "SELECT * FROM channel_setting WHERE channel_id = " + newState.channelId +
+                "&& guild_id =" + newState.guild;
+        console.log("selecting emoji");
+        // execute the query
+        let emoji;
+        dbContext.query(sql, function(err, rows, result){
+            if(err) throw err;
+            try{
+                emoji = rows[0]['specialemoji'];
+            }catch{
+                console.log("Some one joined voice channel: " + newState.channel.name + 
+                "  ,but no channel setting found for this one");
+                return;
+            }
+            
+            // 如果有找到emoji的話
+            if(emoji != undefined)
+            {
+                client.channels.cache.get('638705738314809384').send(emoji + " hi");
+
+
+
+            }
+                
+           
+        });
+
+        
+    })
+
     
+});
+
+// Fire when bot joined a Guild
+client.on('guildCreate', guild =>{
+
+    dbContext.connect(function(err){
+        // create a query to insert a record to audit table and set time = now, type = 3(joinedguild)
+        let timenow = new Date().toISOString().slice(0, 19).replace("T", " ");
+        let sql = "INSERT INTO actionaudit(audit_id,actiontime,actiontype_id) VALUES(default,'" 
+                    + timenow + "',1, NUll, 'client');";
+
+        // execute the query
+        dbContext.query(sql, function(err, result){
+            if(err) throw err;
+            console.log("[server up] audit is inserted");
+        });
+    });
+
 });
 
 
